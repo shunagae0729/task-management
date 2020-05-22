b0594984f3207c95f74039b6f051b79b3ba39fc8
shunagae0729 <chelseashu547@gmail.com>
2020-05-22 17:39:06 +0900

    modefied

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -49,6 +49,7 @@ public class CreateServlet extends HttpServlet {
 
             Timestamp currentTime = new Timestamp(System.currentTimeMillis());
             m.setCreated_at(currentTime);
+            m.setUpdated_at(currentTime);
             List<String> errors = MessageValidator.validate(m);
             if(errors.size() > 0) {
                 em.close();
@@ -68,15 +69,6 @@ public class CreateServlet extends HttpServlet {
 
                 response.sendRedirect(request.getContextPath() + "/index");
             }
-
-            em.getTransaction().begin();
-            em.persist(m);
-            em.getTransaction().commit();
-            request.getSession().setAttribute("flush", "登録が完了しました。");
-            em.close();
-
-            response.sendRedirect(request.getContextPath() + "/index");
         }
     }
-
 }
